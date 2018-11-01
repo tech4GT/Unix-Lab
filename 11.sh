4a8174c37fd823f40407145cae2bcfca4cdd0eab
Varun Gupta <varun.gupta1798@gmail.com>
2018-11-01 11:32:26 +0530

    update
    Signed-off-by: Varun Gupta <varun.gupta1798@gmail.com>

--- 11.sh
+++ 11.sh
@@ -1,7 +1,9 @@
 #Executable not working on winBash
 
+## Can be done with for file in *
+
 files=`ls`
-for file in $files
+for file in files
 do
     [ -w $file ] && [ -f $file ] && echo $file
 done
--- 8.sh
+++ 8.sh
@@ -1,8 +1,6 @@
 
-# echo "Please enter your number: "
-# read num
-
-num=$1
+echo "Please enter your number for factorial: "
+read num
 
 op=1
 for i in `seq $num 2`
